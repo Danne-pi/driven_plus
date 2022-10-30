3e2496427360b985921bc3356ce1874314b04b46
Danne <dannepi.dev@gmail.com>
2022-10-30 17:12:45 -0300

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import SubDetailPage from "./pages/SubDetailPage";
+import SubscriptionPage from "./pages/SubscriptionPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyle>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}/>
+          <Route path="/sign-up" element={<RegisterPage />}/>
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/subscriptions" element={<SubscriptionPage />}/>
+          <Route path="/subscriptions/:SubID" element={<SubDetailPage />}/>
+        </Routes>
+      </BrowserRouter>
+    </AppStyle>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto';
+  }
+`
+
+const AppStyle = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: aqua;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+
+export default function LoginPage (){
+    return( 
+    <LoginPageStyle>
+
+    </LoginPageStyle>
+    )
+}
+
+const LoginPageStyle = styled.div`
+    width: 300px;
+    height: 200px;
+    background-color: white;
+`
